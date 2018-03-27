cf712784fc1ff27fc91ff19ebe6ab29a8b2977f5
Michelle Olivieri <michelleo72@gmail.com>
2018-03-27 20:49:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,15 @@
 class CashRegister
 
+attr_accessor :total
+
 # Hint Keep in mind that to call an instance method inside another instance method,
 # we use the self keyword to refer to the instance on which we are operating
 # For example: self.age += 1
+
+  def initialize
+    @total = 0
+  end
+
+
 
 end
