3d61a7272e9938c861c1262ff7cb87023cbc61aa
Michelle Olivieri <michelleo72@gmail.com>
2018-03-27 21:45:25 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ attr_accessor :total, :discount
   end
 
   def apply_discount
-    self.total -= self.discount
+    self.total.to_f -= self.discount
       "After the discount, the total comes to $#{self.total}."
   end
 
