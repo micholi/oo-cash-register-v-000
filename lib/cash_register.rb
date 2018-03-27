3efe6eecf18fc7c79f7bf2e5f924488df2a31456
Michelle Olivieri <michelleo72@gmail.com>
2018-03-27 20:47:49 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
 
-
+# Hint Keep in mind that to call an instance method inside another instance method,
+# we use the self keyword to refer to the instance on which we are operating
+# For example: self.age += 1
 
 end
-
-  
