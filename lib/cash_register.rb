a1b9a21d7161a54f05cf3d3a63bdd8ab51b40d01
Michelle Olivieri <michelleo72@gmail.com>
2018-03-27 21:51:27 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ attr_accessor :total, :discount
 
   def initialize(discount = 0)
     @total = 0
-    @discount = discount
+    @discount = discount*10
   end
 
   def add_item(title, price, quantity = 1)
